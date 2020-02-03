3ab5587ccdd0c5b73b88f0ef56b52277e6e54b44
Manuel F. Del Toro <mdts.dev@gmail.com>
2020-02-02 23:23:29 -0500

    line width increased to 7.5

--- injectedJavaScript/application.js
+++ injectedJavaScript/application.js
@@ -33,8 +33,8 @@ var content = (penColor, backgroundColor, dataURL) => `
         point.x = translatedX;
         point.y = translatedY;
       }; */
-      signaturePad.minWidth = 6.5;
-      signaturePad.maxWidth = 7.5;
+      signaturePad.minWidth = 7.5;
+      signaturePad.maxWidth = 10.5;
       if ('${dataURL}') {
         signaturePad.fromDataURL('${dataURL}');
       }
